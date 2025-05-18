583504b9a31f9fc7be3e46784864e07b7c1e22ad
Wilmer Vnr <wilmer.vargas@epn.edu.ec>
2025-05-18 15:16:16 -0500

    chat en tiempo real

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import app from './server.js'
-import connection from './database.js';
+import connection from './database.js'
+import http from 'http'
+import { Server } from 'socket.io'
 
-app.listen(app.get('port'),()=>{
-    console.log(`Server ok on http://localhost:${app.get('port')}`);
-})
 
 connection()
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173"
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log('Usuario conectado',socket.id)
+    socket.on('enviar-mensaje-front-back',(payload)=>{
+        socket.broadcast.emit('enviar-mensaje-front-back',payload)
+    })
+})
+
+
+
+server.listen(app.get('port'),()=>{
+    console.log(`Server ok on http://localhost:${app.get('port')}`);
+})
