b4215783769c2d732651bdc2261bf705fad09848
Wilmer Vnr <wilmer.vargas@epn.edu.ec>
2025-06-27 00:52:34 -0500

    mejoras

--- src/routers/Paciente_routes.js
+++ src/routers/Paciente_routes.js
@@ -1,7 +1,7 @@
 import {Router} from 'express'
 import { actualizarAvatar, actualizarPassword, actualizarPerfil, confirmarMail, detalleComidasPaciente, detalleParametrosPaciente, listarNutricionistas, perfil,registro } from '../controllers/Paciente_controller.js'
 import { verificarTokenJWT } from '../middlewares/JWT.js'
-import { validacionPaciente } from '../middlewares/validacionPaciente.js'
+import { validacionPaciente, validacionPerfil } from '../middlewares/validacionPaciente.js'
 import { validarActualizarPassword} from '../middlewares/validacionPassword.js'
 const router = Router()
 
@@ -12,7 +12,7 @@ router.get('/confirmar/:token',confirmarMail)
 
 // Ruta para ver y actualizar perfil del paciente
 router.get('/perfil', verificarTokenJWT, perfil)
-router.put('/paciente/:id',verificarTokenJWT,validacionPaciente,actualizarPerfil)
+router.put('/paciente/:id',verificarTokenJWT,validacionPerfil,actualizarPerfil)
 router.put('/paciente/actualizar-password/:id',verificarTokenJWT,validarActualizarPassword,actualizarPassword)
 router.get('/paciente/parametro/:id', verificarTokenJWT, detalleParametrosPaciente)
 router.get('/paciente/comidas/:id', verificarTokenJWT, detalleComidasPaciente)
